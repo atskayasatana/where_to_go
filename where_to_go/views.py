0daa3c7afc468c8041758c5f4dbb5748eb95b9e9
atskayasatana <88509622+atskayasatana@users.noreply.github.com>
2023-06-13 22:56:01 +0300

    get geo_json from DB

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,9 @@
 # views.py
 
+import json
+import pprint
+
+from collections import OrderedDict
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
@@ -13,24 +17,27 @@ def show_main_page(request):
     places_set = Place.objects.all()
 
     for place in places_set.iterator():
-        geometry_dict = dict.fromkeys({'type', 'coordinates'})
-        properties_dict = dict.fromkeys({'title', 'placeId', 'detailsUrl'})
-        geo_json_features_dict = dict.fromkeys({'type', 'geometry', 'properties'})
+
+        feature_dict =  OrderedDict()
+        geometry_dict = OrderedDict()
+        properties_dict = OrderedDict()
+
 
         title = place.title
-        coordinates = [place.latitude, place.longtitude]
+        coordinates = [place.longtitude, place.latitude]
 
         geometry_dict['type'] = 'Point'
         geometry_dict['coordinates'] = coordinates
 
-        properties_dict['title'] = place.title
-        properties_dict['placeId'] = place.title
+        properties_dict['title'] = title
+        properties_dict['placeId'] = title
         properties_dict['detailsUrl'] = 'XXX'
 
-        geo_json_features_dict['type'] = 'Feature'
-        geo_json_features_dict['geometry'] = geometry_dict
-        geo_json_features_dict['properties'] = properties_dict
+        feature_dict['type'] = 'Feature'
+        feature_dict['geometry'] = geometry_dict
+        feature_dict['properties'] =properties_dict
 
-        geo_json_dict['features'].append(geo_json_features_dict)
+        geo_json_dict['features'].append(dict(feature_dict))
 
-    return render(request, 'index.html')
+
+    return render(request, 'index.html', {'value':geo_json_dict})
